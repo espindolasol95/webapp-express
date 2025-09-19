302c5fe44607467082185620a0d0e4ee4a5c829b
espindolasol95 <espindolasol.ok@gmail.com>
2025-09-19 15:07:08 +0200

    feat(definisco la rotta base e dico al server di rimanere in ascolto  )

--- app.js
+++ app.js
@@ -7,3 +7,16 @@ const app = express()
 
 // definisco il numero di posrta su qui deve girare  l'applicazione
 const port = 3000
+
+// definisco la rotta base 
+app.get("/",(req,res) => {
+    res.send ("Rotta base del mio blog")
+
+})
+
+
+//dico al server di rimanere in ascolto 
+
+app.listen(port,()=>{
+console.log(`server in ascolto sulla porta ${port}`)
+})
