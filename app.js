67c7e16211d7ba34bb3a05c2875099030901dff7
espindolasol95 <espindolasol.ok@gmail.com>
2025-09-19 14:55:45 +0200

    feat( in file app.js importo express, creo l'instanza dell app atraverso  metodo express che ho creato e definisco il numero di posrta su qui deve girare  l'applicazione)

---
+++ app.js
@@ -0,0 +1,9 @@
+//importo express
+const express = require (expres)
+
+//creo l'instanza dell app atraverso  metodo express che ho creato
+
+const app = express()
+
+// definisco il numero di posrta su qui deve girare  l'applicazione
+const port = 3000
