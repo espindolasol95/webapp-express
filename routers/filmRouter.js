26ebf511de9efac6c26724a67397b9e0eac9e732
espindolasol95 <espindolasol.ok@gmail.com>
2025-09-21 20:56:51 +0200

    feat(creazione file routers importazione di router,e definizione delle rotte index show )

---
+++ routers/filmRouter.js
@@ -0,0 +1,16 @@
+//importo express 
+
+const express = require('express')
+
+//importo il router
+const router = express.Router()
+
+//importo controler 
+const filmController = require ('../controllers/filmController')
+
+// definizione delle rotte
+//index
+router.get('/',filmController.index)
+
+//show
+router.get('/:id',filmController.show)
