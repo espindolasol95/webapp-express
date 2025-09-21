d6ef467b121b0f817e8a31a3050192d56f3e833a
espindolasol95 <espindolasol.ok@gmail.com>
2025-09-22 00:32:34 +0200

    feat(creazione query del reviews,oggetto contenente i dati dei film e l'array delle recensioni)

--- controllers/filmController.js
+++ controllers/filmController.js
@@ -17,10 +17,34 @@ const show = (req,res) =>{
     const{id} = req.params
     //creo la query
     const sqlMovies = 'SELECT * FROM movies WHERE id =?'
-     //passo i parametri
-     conection.query(sqlMovies,[id],(err, resultMovies) => {
-        if (err) return res. status(500).json({error:`errore nella esecuzione : ${err}`} )
-     res.send(resultMovies[0])
+
+    // creo la query per le ricenzioni 
+     const sqlReviews = 'SELECT * FROM reviews WHERE id =?'
+
+
+    //passo i parametri
+    conection.query(sqlMovies,[id],(err, resultMovies) => {
+        if (err) return res.status(500).json({error:`errore nella esecuzione : ${err}`} )
+
+
+    //verifico di aver trovato il lìfilm
+     if (resultMovies.length ===0 ||resultMovies [0].id === null) return res.status
+     (404).json ({error : 'film non trovato'})
+
+    //eseguo la query del reviews
+     conection.query(sqlReviews,[id],(err,resulReviews) => {
+         if (err) return res.status(500).json({error:`errore nella esecuzione :${err}`} )
+            
+    // creo un nuovo oggetto contenente i dati dei film e l'array delle recensioni 
+       const filmWithreviews = {
+        ...resultMovies[0],reviews: resulReviews
+       }
+        
+       res.send (filmWithreviews)
+
+     })
+
+     
     })
 
 }
