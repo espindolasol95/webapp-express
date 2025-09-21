1d330985db42b22420431a309d5e573be1ded0e4
espindolasol95 <espindolasol.ok@gmail.com>
2025-09-21 19:45:18 +0200

    feat(creazione  file filmcontrolers con il mettodo index)

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ require('dotenv').config()
 //importo express
 const express = require ('express')
 
-const conection = require ('./data/db')
-
 //creo l'instanza dell app atraverso  metodo express che ho creato
 
 const app = express()
 
 // definisco il numero di posrta su qui deve girare  l'applicazione
 const port = process.env.PORT||3000
+
+app.use(express.static ('public'))
+
 // definisco la rotta base 
 app.get("/",(req,res) => {
     res.send ("Rotta base del mio blog")
---
+++ controllers/filmController.js
@@ -0,0 +1,11 @@
+// importare la conessione al db
+
+const conection = require ('../data/db')
+
+// index
+
+const index = (req,res) => {
+    console.log('metodo index')
+
+} 
+//show
