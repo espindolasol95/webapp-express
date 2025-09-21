1c712e3674a60886d7879014b54b6dc3f1165986
espindolasol95 <espindolasol.ok@gmail.com>
2025-09-21 16:10:26 +0200

    test(configurazione e correzione  del file.env )

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
+require('dotenv').config()
 //importo express
 const express = require ('express')
 
-//const conection = require ('./data/db')
+const conection = require ('./data/db')
 
 //creo l'instanza dell app atraverso  metodo express che ho creato
 
 const app = express()
 
 // definisco il numero di posrta su qui deve girare  l'applicazione
-const port = process.env.PORT
-
+const port = process.env.PORT||3000
 // definisco la rotta base 
 app.get("/",(req,res) => {
     res.send ("Rotta base del mio blog")
@@ -20,5 +20,5 @@ app.get("/",(req,res) => {
 //dico al server di rimanere in ascolto 
 
 app.listen(port,()=>{
-console.log(`server in ascolto sulla porta ${port}`)
+console.log(`Server in ascolto sulla porta ${port}`)
 })
--- data/db.js
+++ data/db.js
@@ -7,14 +7,14 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_NAME,
     port: process.env.DB_PORT
 })
 //stabilisco la connesione al db
 connection.connect ((err)=>{
 if(err){
-    console.log(`errore nella connesione al db ${err}`)
+    console.log(`Errore nella connesione al db ${err}`)
 }else{
-    console.log('connesione al db avvenuta correttamente')
+    console.log('Connesione al db avvenuta correttamente')
 }
 })
